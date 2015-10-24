aa307d8ffff877630b2f44995178b864ce5bb65f
Matthew Wo <9029537@gmail.com>
2015-10-24 12:41:42 -0700

    Minor changes

--- LinkedInForStudents/client/panels/HomePanel.js
+++ LinkedInForStudents/client/panels/HomePanel.js
@@ -4,16 +4,28 @@ Template.HomePanel.onRendered(function(){
 
 Template.HomePanel.helpers({
   teachersResults: function() {
-    return Teachers.find({name: Session.get('home-query')})
+    return Teachers.find({$or: [{"FirstName":{'$regex':Session.get('home-query')}},{"LastName":{'$regex':Session.get('home-query')}}]})
   },
   schoolsResults: function() {
-    return Schools.find({name: Session.get('home-query')})
+    return Schools.find({SchoolName: {'$regex':Session.get('home-query')}})
+  },
+  isQuerying: function() {
+    console.log("checking")
+    return Session.get('is-querying');
   }
 })
 
 Template.HomePanel.events({
-  'keyup #query': function(e) {
+  'keyup #query': function() {
     Session.set('home-query', $('#query').val());
     // console.log($('#query').val())
+  },
+  'focus #query': function() {
+    console.log("focused")
+    Session.set('is-querying', true);
+  },
+  'blur #query': function() {
+    console.log('blurred');
+    Session.set('is-querying', false);
   }
 })
