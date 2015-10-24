72a5fce0f23eb552744138c9b9acbc838a2c9934
jason <huang5640@live.com>
2015-10-24 16:21:20 -0700

    Add the feature of showing all students' grades for one school

--- LinkedInForStudents/server/yummy.js
+++ LinkedInForStudents/server/yummy.js
@@ -4,6 +4,7 @@ Student = new Mongo.Collection('student');
 Achievement = new Mongo.Collection('achievement');
 Test = new Mongo.Collection('test');
 Comment = new Mongo.Collection('comment');
+SchoolGrades = new Mongo.Collection('schoolGrades');
 
 	if (Teacher.find().count() === 0) {
         var data = JSON.parse(Assets.getText("teacher.json"));
@@ -45,10 +46,18 @@ Comment = new Mongo.Collection('comment');
         })
     }
 
+    if (SchoolGrades.find().count() === 0) {
+        var data = JSON.parse(Assets.getText("schoolGrades.json"));
+
+        data.forEach(function (item, index, array) {
+            SchoolGrades.insert(item);
+        })
+    }
+
     //These code add the teacher to the Schools
     schools = School.find({}).fetch()
     teachers = Teacher.find({}).fetch()
-    for (var i = 0; j < teachers.length; i++)
+    for (var i = 0; i < teachers.length; i++)
     {
         for (var j = 0; j < schools.length; j++) 
         {
@@ -75,3 +84,59 @@ Comment = new Mongo.Collection('comment');
             }
         }
     }
+
+    //These code should count the num of each grade
+    schoolGrades = SchoolGrades.find({}).fetch() //an array
+    tests = Test.find({}).fetch() //an array
+    for (var i = 0; i < schools.length; i++)
+    {
+        //this step gives me a School
+        var school = schools[i]
+        var gradeArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        for (var j = 0; j < school.Teachers.length; j++)
+        {
+            //this step gives me a teacher
+            console.log(j)
+            var teacher = Teacher.findOne({TeacherID: school.Teachers[j]})
+            for (var k = 0; k < teacher.Tests.length; k++)
+            {
+                //this step gives me a list of score
+                var scores = tests[k].Scores 
+                for (var m = 0; m < scores.length; m++)
+                {
+                    //this give me a single score
+                    var score = scores[m]
+                    var index
+                    if (score > 98)
+                        index = 0 //A+
+                    else if (score > 92)
+                        index = 1 //A
+                    else if (score > 90)
+                        index = 2 //A-
+                    else if (score > 87)
+                        index = 3 //B+
+                    else if (score > 85)
+                        index = 4 //B
+                    else if (score > 80)
+                        index = 5 //B-
+                    else if (score > 77)
+                        index = 6 //C+
+                    else if (score > 75)
+                        index = 7 //C
+                    else if (score > 70)
+                        index = 8 //C-
+                    else if (score > 60)
+                        index = 9 //D
+                    else
+                        index = 10 //F
+
+                    //then I update it
+                    gradeArray[index]++
+                }
+                
+            }
+            console.log(gradeArray);
+        }
+        //insert the array to it
+        SchoolGrades.update({SchoolID: school.SchoolID}, {$set: {Grades: gradeArray}})
+   }
