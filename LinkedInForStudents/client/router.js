16d6896b57eb34054c2f315bf83d877116f67b1b
Haofei Chen <phoenix0722chen@gmail.com>
2015-10-24 03:03:16 -0700

    Added sidebar and profile page

--- LinkedInForStudents/client/router.js
+++ LinkedInForStudents/client/router.js
@@ -6,6 +6,12 @@ Router.route('/test-people', function(){
   this.render('LivePanel')
 });
 
+Router.route('/profile-people', function(){
+  this.render('ProfilePanel')
+  this.render('ProfileSide',{to:"aside"})
+});
+
+
 Router.route('/', function(){
   this.render('HomePanel')
 })
