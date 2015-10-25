5c29172d5548a75f31f1e329c2c717300fb1a995
Haofei Chen <phoenix0722chen@gmail.com>
2015-10-24 23:57:11 -0700

    update everything

--- LinkedInForStudents/client/router.js
+++ LinkedInForStudents/client/router.js
@@ -11,6 +11,16 @@ Router.route('/profile-people', function(){
   this.render('ProfileSide',{to:"aside"})
 });
 
+Router.route('/profile-school', function(){
+  this.render('ProfilePanelSchool')
+  this.render('SchoolSide',{to:"aside"})
+});
+
+Router.route('/Setting', function(){
+  this.layout('HomeLayout')
+  this.render('SettingPanel')
+});
+
 Router.route('/search/:searchType/:searchQuery', function(){
 	this.render('SearchResults',{
 		data: function (){ return {searchQuery:this.params.searchQuery, searchType:this.params.searchType}}});
@@ -19,5 +29,6 @@ Router.route('/search/:searchType/:searchQuery', function(){
 );
 
 Router.route('/', function(){
+  this.layout('HomeLayout')
   this.render('HomePanel')
 })
