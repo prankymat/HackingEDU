21a22b7e9b51c98d6dd01a5ba4b5b8a3f066d1d9
Brian <bquock@usc.edu>
2015-10-25 02:55:03 -0700

    Merged Phoenix in

--- client/router.js
+++ client/router.js
@@ -13,6 +13,16 @@ Router.route('/profile-people/:aTeacherID', function(){
 	data: function (){ return {aTeacherID:this.params.aTeacherID}}})
 });
 
+Router.route('/profile-school', function(){
+  this.render('ProfilePanelSchool')
+  this.render('SchoolSide',{to:"aside"})
+});
+
+Router.route('/Setting', function(){
+  this.layout('HomeLayout')
+  this.render('SettingPanel')
+});
+
 Router.route('/search/:searchQuery', function(){
   this.layout('HomeLayout')
 	this.render('SearchResults',{
